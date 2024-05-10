722a6d2e1897d4634b2eb6003edc0b4b9a98aa8e
Jaya Raj Srivathsav Adari <ajsrivathsav3502@gmail.com>
2024-05-10 15:11:14 -0400

    update

--- svm.py
+++ svm.py
@@ -1,48 +1,31 @@
 from __future__ import division
-import numpy as np
+import numpy
 import os
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
+from sklearn import svm
+from collections import deque
+from sklearn import tree
+
+
 
 class SVM:
 
     def __init__(self):
         """
-        Train the model from generated training data.
+        train the model from generated training data in generate-data folder
         """
-        data = np.loadtxt('result.csv', delimiter=',')
-        X = data[:, 0:3]
-        y = data[:, 3].astype(int)
+        data = numpy.loadtxt(open('result.csv', 'rb'), delimiter=',', dtype='str')
+        #Support_Vector_Machine
+        #self.svm = svm.SVC()
+        # Decision tree
+        self.svm = tree.DecisionTreeClassifier()
+        self.svm.fit(data[:, 0:3], data[:, 3])
 
-        # Data preprocessing and model pipeline
-        pipeline = Pipeline([
-            ('scaler', StandardScaler()),
-            ('classifier', RandomForestClassifier())
-        ])
-
-        # Grid search to find best parameters
-        param_grid = {
-            'classifier__n_estimators': [100, 200, 300],
-            'classifier__max_depth': [None, 10, 20, 30],
-            'classifier__min_samples_split': [2, 5, 10]
-        }
-
-        grid_search = GridSearchCV(pipeline, param_grid, cv=5)
-        grid_search.fit(X, y)
-        self.model = grid_search.best_estimator_
 
     def classify(self, data):
-        """
-        Classify new instances using the trained model.
-        """
-        try:
-            fparams = np.array(data).reshape(1, -1)
-            prediction = self.model.predict(fparams)
-            print("Input data:", data, "Prediction result:", prediction)
-            return prediction
-        except Exception as e:
-            print("Error during classification:", e)
-            return None 
-
+        fparams = numpy.zeros((1, 3))
+        fparams[:,0] = data[0]
+        fparams[:,1] = data[1]
+        fparams[:,2] = data[2]
+        prediction = self.svm.predict(fparams)
+        print("SVM input data", data , "prediction result ", prediction)
+        return prediction
